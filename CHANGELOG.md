ad62ed86a1c6661f2f34a54fee4696823f8cbbcc
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-12 21:31:14 +0000

    Feature/qn (#3)
    * feature/qn: some refactoring
    
    * feature/qn: implementation of 'qn' and tests for it
    
    * feature/qn: more tests and docs
    
    * Create CHANGELOG.md
    
    * feature/qn: README.md updated

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.1.0
+
+- Added `qn` QuasiQuoter as alternative to `qm` but without interpolation
+- Some code refactoring
+- More docs and tests
--- src/Text/InterpolatedString/QM.hs
+++ src/Text/InterpolatedString/QM.hs
@@ -13,12 +13,12 @@
 
 {-# LANGUAGE CPP #-}
 
-module Text.InterpolatedString.QM (qm, ShowQ(..)) where
+module Text.InterpolatedString.QM (qm, qn, ShowQ(..)) where
 
-import "base" GHC.Exts (IsString(..))
+import "base" GHC.Exts (IsString (fromString))
 import qualified "template-haskell" Language.Haskell.TH as TH
-import "template-haskell" Language.Haskell.TH.Quote
-import "haskell-src-meta" Language.Haskell.Meta.Parse
+import "template-haskell" Language.Haskell.TH.Quote (QuasiQuoter (QuasiQuoter))
+import "haskell-src-meta" Language.Haskell.Meta.Parse (parseExp)
 import "bytestring" Data.ByteString.Char8 as Strict (ByteString, unpack)
 import "bytestring" Data.ByteString.Lazy.Char8 as Lazy (ByteString, unpack)
 import "text" Data.Text as T (Text, unpack)
@@ -67,49 +67,69 @@ data StringPart = Literal String | AntiQuote String deriving Show
 
 
 unQM :: String -> String -> [StringPart]
-unQM a []          = [Literal (reverse a)]
+unQM a ""          = [Literal (reverse a)]
 unQM a ('\\':x:xs) = unQM (x:a) xs
-unQM a ("\\")      = unQM ('\\':a) []
-unQM a ('}':xs)    = AntiQuote (reverse a) : parseQM [] xs
+unQM a ("\\")      = unQM ('\\':a) ""
+unQM a ('}':xs)    = AntiQuote (reverse a) : parseQM "" xs
 unQM a (x:xs)      = unQM (x:a) xs
 
+
 parseQM :: String -> String -> [StringPart]
-parseQM a []             = [Literal (reverse a)]
+parseQM a ""             = [Literal (reverse a)]
 parseQM a ('\\':'\\':xs) = parseQM ('\\':a) xs
 parseQM a ('\\':'{':xs)  = parseQM ('{':a) xs
 parseQM a ('\\':' ':xs)  = parseQM (' ':a) xs
 parseQM a ('\\':'\n':xs) = parseQM a ('\n':xs)
 parseQM a ('\\':'n':xs)  = parseQM ('\n':a) xs
-parseQM a ("\\")         = parseQM ('\\':a) []
-parseQM a ('{':xs)       = Literal (reverse a) : unQM [] xs
+parseQM a ("\\")         = parseQM ('\\':a) ""
+parseQM a ('{':xs)       = Literal (reverse a) : unQM "" xs
 parseQM a (clearIndentAtSOF   -> Just clean) = parseQM a clean
 parseQM a (clearIndentTillEOF -> Just clean) = parseQM a clean
 parseQM a ('\n':xs)      = parseQM a xs -- cut off line breaks
 parseQM a (x:xs)         = parseQM (x:a) xs
 
+
+parseQN :: String -> String -> [StringPart]
+parseQN a ""             = [Literal (reverse a)]
+parseQN a ('\\':'\\':xs) = parseQN ('\\':a) xs
+parseQN a ('\\':' ':xs)  = parseQN (' ':a) xs
+parseQN a ('\\':'\n':xs) = parseQN a ('\n':xs)
+parseQN a ('\\':'n':xs)  = parseQN ('\n':a) xs
+parseQN a ("\\")         = parseQN ('\\':a) ""
+parseQN a (clearIndentAtSOF   -> Just clean) = parseQN a clean
+parseQN a (clearIndentTillEOF -> Just clean) = parseQN a clean
+parseQN a ('\n':xs)      = parseQN a xs -- cut off line breaks
+parseQN a (x:xs)         = parseQN (x:a) xs
+
+
 clearIndentTillEOF :: String -> Maybe String
-clearIndentTillEOF str@((ifMaybe (`elem` "\t ") -> Just _) : _) = cutOff str
-  where cutOff :: String -> Maybe String
-        cutOff ""            = Just ""
-        cutOff eof@('\n':_) = Just eof
-        cutOff ((ifMaybe (`elem` "\t ") -> Just _) : xs) = cutOff xs
-        cutOff _ = Nothing
-clearIndentTillEOF _ = Nothing
+clearIndentTillEOF s | s == ""             = Nothing
+                     | head s `elem` "\t " = cutOff s
+                     | otherwise           = Nothing
+
+  where cutOff x | x == ""             = Just ""
+                 | head x == '\n'      = Just x
+                 | head x `elem` "\t " = cutOff $ tail x
+                 | otherwise           = Nothing
+
 
 clearIndentAtSOF :: String -> Maybe String
-clearIndentAtSOF ('\n' : xs) = if result /= xs
-                                  then Just $ '\n' : cutOff xs
-                                  else Nothing
-  where cutOff :: String -> String
-        cutOff ((ifMaybe (`elem` "\t ") -> Just _) : ys) = cutOff ys
-        cutOff s = s
-        result = cutOff xs
-clearIndentAtSOF _ = Nothing
+clearIndentAtSOF s | s == ""                      = Nothing
+                   | head s == '\n' && hasChanges = Just processed
+                   | otherwise                    = Nothing
+
+  where processed  = '\n' : cutOff (tail s)
+        hasChanges = processed /= s
+
+        cutOff x | x == ""             = ""
+                 | head x `elem` "\t " = cutOff $ tail x
+                 | otherwise           = x
+
 
 clearIndentAtStart :: String -> String
-clearIndentAtStart ((ifMaybe (`elem` "\t ") -> Just _) : xs) =
-  clearIndentAtStart xs
-clearIndentAtStart s = s
+clearIndentAtStart s | s == ""             = ""
+                     | head s `elem` "\t " = clearIndentAtStart $ tail s
+                     | otherwise           = s
 
 
 makeExpr :: [StringPart] -> TH.ExpQ
@@ -119,20 +139,58 @@ makeExpr (Literal a : xs) =
 makeExpr (AntiQuote a : xs) =
   TH.appE [| mappend (toQQ $(reify a)) |] $ makeExpr xs
 
+
 reify :: String -> TH.Q TH.Exp
-reify s =
-  case parseExp s of
-       Left  e -> TH.reportError e >> [| mempty |]
-       Right e -> return e
+reify s = case parseExp s of
+               Left  e -> TH.reportError e >> [| mempty |]
+               Right e -> return e
 
 
+-- | QuasiQuoter for multiline interpolated string.
+--
+-- @
+-- [qm| foo {'b':'a':'r':""}
+--    \\ baz |] -- "foo bar baz"
+-- @
+--
+-- Symbols that could be escaped:
+--
+--   * @\\@ - backslash itself (two backslashes one by one: @\\\\@)
+--     @[qm| foo\\\\bar |] -- "foo\\\\bar"@
+--
+--   * Space symbol at the edge
+--     (to put it to the output instead of just ignoring it)
+--     @[qm| foo\\ |] -- "foo "@ or @[qm|\\ foo |] -- " foo"@
+--
+--   * Line break @\\n@ (actual line breaks are ignored)
+--
+--   * Opening bracket of interpolation block @\\{@
+--     to prevent interpolatin and put it as it is
+--     @[qm| {1+2} \\{3+4} |] -- "3 {3+4}"@
+--
 qm :: QuasiQuoter
 qm = QuasiQuoter f
   (error "Cannot use qm as a pattern")
   (error "Cannot use qm as a type")
   (error "Cannot use qm as a dec")
-  where f = makeExpr . parseQM [] . clearIndentAtStart . filter (/= '\r')
+  where f = makeExpr . parseQM "" . clearIndentAtStart . filter (/= '\r')
 
 
-ifMaybe :: (a -> Bool) -> a -> Maybe a
-ifMaybe f x = if f x then Just x else Nothing
+-- | Works just like `qm` but without interpolation
+--   (just multiline string with decorative indentation).
+--
+-- @
+-- [qn| foo {'b':'a':'r':""}
+--    \\ baz |] -- "foo {'b':'a':'r':\\"\\"} baz"
+-- @
+--
+-- Interpolation blocks goes just as text:
+--
+-- @[qn| {1+2} \\{3+4} |] -- "{1+2} \\\\{3+4}"@
+--
+qn :: QuasiQuoter
+qn = QuasiQuoter f
+  (error "Cannot use qn as a pattern")
+  (error "Cannot use qn as a type")
+  (error "Cannot use qn as a dec")
+  where f = makeExpr . parseQN "" . clearIndentAtStart . filter (/= '\r')
