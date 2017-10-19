da28389f3a4ae34db217a3ab156bf4e751cbc38d
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-10-20 00:02:33 +0500

    added haddocks for new quoters

--- src/Text/InterpolatedString/QM.hs
+++ src/Text/InterpolatedString/QM.hs
@@ -15,24 +15,24 @@ import qualified Text.InterpolatedString.QM.Internal.Parsers as Parsers
 -- | QuasiQuoter for multiline interpolated string.
 --
 -- @
--- [qm| foo {'b':'a':'r':""}
+-- ['qm'| foo {'b':'a':'r':""}
 --    \\ baz |] -- "foo bar baz"
 -- @
 --
 -- Symbols that could be escaped:
 --
 --   * @\\@ - backslash itself (two backslashes one by one: @\\\\@)
---     @[qm| foo\\\\bar |] -- "foo\\\\bar"@
+--     @['qm'| foo\\\\bar |] -- "foo\\\\bar"@
 --
 --   * Space symbol at the edge
 --     (to put it to the output instead of just ignoring it)
---     @[qm| foo\\ |] -- "foo "@ or @[qm|\\ foo |] -- " foo"@
+--     @['qm'| foo\\ |] -- "foo "@ or @['qm'|\\ foo |] -- " foo"@
 --
 --   * Line break @\\n@ (actual line breaks are ignored)
 --
 --   * Opening bracket of interpolation block @\\{@
 --     to prevent interpolatin and put it as it is
---     @[qm| {1+2} \\{3+4} |] -- "3 {3+4}"@
+--     @['qm'| {1+2} \\{3+4} |] -- "3 {3+4}"@
 --
 qm :: QuasiQuoter
 qm = QuasiQuoter Parsers.qm
@@ -41,17 +41,17 @@ qm = QuasiQuoter Parsers.qm
   (error "Cannot use 'qm' as a dec")
 
 
--- | Works just like `qm` but without interpolation
+-- | Works just like 'qm' but without interpolation
 --   (just multiline string with decorative indentation).
 --
 -- @
--- [qn| foo {'b':'a':'r':""}
+-- ['qn'| foo {'b':'a':'r':""}
 --    \\ baz |] -- "foo {'b':'a':'r':\\"\\"} baz"
 -- @
 --
 -- Interpolation blocks goes just as text:
 --
--- @[qn| {1+2} \\{3+4} |] -- "{1+2} \\\\{3+4}"@
+-- @['qn'| {1+2} \\{3+4} |] -- "{1+2} \\\\{3+4}"@
 --
 qn :: QuasiQuoter
 qn = QuasiQuoter Parsers.qn
@@ -60,7 +60,14 @@ qn = QuasiQuoter Parsers.qn
   (error "Cannot use 'qn' as a dec")
 
 
--- TODO add description
+-- | 'qm' + `b` (line-/B/reaks)
+--
+-- @
+-- ['qmb'| foo
+--       {'b':'a':'r':""}
+--       baz |] -- "foo\\nbar\\nbaz"
+-- @
+--
 qmb :: QuasiQuoter
 qmb = QuasiQuoter Parsers.qmb
   (error "Cannot use 'qmb' as a pattern")
@@ -68,7 +75,14 @@ qmb = QuasiQuoter Parsers.qmb
   (error "Cannot use 'qmb' as a dec")
 
 
--- TODO add description
+-- | 'qn' + `b` (line-/B/reaks)
+-- Works just like 'qmb' but without interpolation.
+--
+-- @
+-- ['qnb'| foo
+--       {'b':'a':'r':""}
+--       baz |] -- "foo\\n{'b':'a':'r':\\"\\"}\\nbaz"
+-- @
 qnb :: QuasiQuoter
 qnb = QuasiQuoter Parsers.qnb
   (error "Cannot use 'qnb' as a pattern")
@@ -76,7 +90,13 @@ qnb = QuasiQuoter Parsers.qnb
   (error "Cannot use 'qnb' as a dec")
 
 
--- TODO add description
+-- | 'qm' + `s` (/S/paces)
+--
+-- @
+-- ['qms'| foo
+--       {'b':'a':'r':""}
+--       baz |] -- "foo bar baz"
+-- @
 qms :: QuasiQuoter
 qms = QuasiQuoter Parsers.qms
   (error "Cannot use 'qms' as a pattern")
@@ -84,7 +104,15 @@ qms = QuasiQuoter Parsers.qms
   (error "Cannot use 'qms' as a dec")
 
 
--- TODO add description
+-- | 'qn' + `s` (/S/paces)
+--
+-- Works just like 'qms' but without interpolation.
+--
+-- @
+-- ['qns'| foo
+--       {'b':'a':'r':""}
+--       baz |] -- "foo {'b':'a':'r':\\"\\"} baz"
+-- @
 qns :: QuasiQuoter
 qns = QuasiQuoter Parsers.qns
   (error "Cannot use 'qns' as a pattern")
